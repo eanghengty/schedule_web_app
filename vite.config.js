89b544eb1dd6010f55372060275af8bec94425a3
Hengty (Jack) Eang <hengty.eang@skyaus.com.au>
2026-05-02 22:39:03 +0700

    update vite file by chnage schedule app to repo name

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/schedule_web_app/',   // ← replace with your exact GitHub repo name
   server: {
     port: 6003,
   },
